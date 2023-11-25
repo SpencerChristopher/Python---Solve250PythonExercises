850b9da32923f023e9765099790eb6110f71f37f
chris <Spencer.Christopher.g@gmail.com>
2023-11-25 20:25:29 +0100

    added solution to exercise 1 print last charater of sting

---
+++ Exercise1/Exercise1Solution.py
@@ -0,0 +1,16 @@
+'''
+Module descrition:
+solution to exercise 1
+print the last character of a sting
+
+'''
+
+# Main function
+def main():
+    my_sting = "I love python"
+    print(my_sting)
+    print(f'The last character of I love python {my_sting[-1]}')
+
+# Execution starts here
+if __name__ == "__main__":
+    main()
